0aeeb35e70fe61a21cf958785971334038d4b44e
bharath kumar <jdbharath08@gmail.com>
2024-02-05 12:49:15 +0530

    test

---
+++ src/assets/data/users.ts
@@ -0,0 +1,25 @@
+/* tslint:disable max-line-length */
+import { User } from '../../app/shared/models/user';
+
+const users: User[] = [
+  {
+    id: 1,
+    name: 'bharath',
+    email: 'netflix@netflix.com',
+    password: '12345678',
+    avatar: 'assets/profile1.png',
+    plan: 'Premium Ultra HD',
+    creditCard: '8989',
+  },
+  {
+    id: 1,
+    name: 'kido',
+    email: 'kio@netflix.com',
+    password: 'kido123',
+    avatar: 'assets/profile2.png',
+    plan: 'Premium HD',
+    creditCard: '4545',
+  },
+];
+
+export default users;
